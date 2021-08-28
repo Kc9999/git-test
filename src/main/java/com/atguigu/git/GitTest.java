be644206386f05bed9cc8af5d11c89b85970a6f7
kcjia <1455386918@qq.com>
2021-08-28 16:41:52 +0800

    first commit idea

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @author xf.Z
+ * @create 2021-08-28 16:35
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
